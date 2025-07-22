436aaf6403960c4f4c95f4924c6b4da568571e39
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-22 17:52:24 +0300

    Part 01 Abstraction [Class, Method, Property]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace Demo
+﻿using Demo.Abstraction;
+
+namespace Demo
 {
     internal class Program
     {
+        #region Abstract [Abstract Class, Abstract Method, Abstract Property]
+        static void PrintShapeInfo(Shape shape)
+        {
+            Console.WriteLine($"shape.Dim01 {shape.Dim01}");
+            Console.WriteLine($"shape.Dim02 {shape.Dim02}");
+            Console.WriteLine($"shape.CalcArea = {shape.CalcArea()}");
+            Console.WriteLine($"shape.Perimeter = {shape.Perimeter}");
+        }
+        static void Draw2D(IDraw2D shape)
+        {
+            shape.Draw();
+        } 
+        #endregion
         static void Main()
         {
-            
+            #region Abstract [Abstract Class, Abstract Method, Abstract Property]
+            /// Shape shape;
+            /// /// shape.CalcArea();
+            /// 
+            /// shape = new Rectangle(10, 20);
+            /// 
+            /// decimal rectArea = shape.CalcArea();
+            /// decimal rectPeri = shape.Perimeter;
+            /// 
+            /// Console.WriteLine($"rect.CalcArea() => {rectArea}");
+            /// Console.WriteLine($"rectPeri.Perimeter => {rectPeri}");
+            /// 
+            /// shape = new Square(20);
+            /// 
+            /// decimal squarePeria = shape.Perimeter;
+            /// decimal squareArea = shape.CalcArea();
+            /// 
+            /// Console.WriteLine($"{squarePeria}");
+            /// Console.WriteLine($"{squareArea}");
+            /// 
+            /// Circle circle = new Circle(10);
+            /// Console.WriteLine(circle.Perimeter);
+            /// Console.WriteLine(circle.CalcArea());
+            /// 
+            /// PrintShapeInfo(shape); 
+            #endregion
+
+
+
         }
     }
 }
