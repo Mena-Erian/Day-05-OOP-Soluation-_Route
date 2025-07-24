c7c5d71919e9b76829916ffdb4e9bb2544312563
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-24 18:08:00 +0300

    Create Point 3D class and helper class and making from task 1 to 3 [casting]

---
+++ Assignment/First Task/Point3D.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.First_Task
+{
+    internal class Point3D 
+    {
+        #region Properties
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        #endregion
+
+        #region Constructors
+        public Point3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public Point3D(int x, int y) : this(x, y, 0)
+        {
+        }
+        public Point3D(int x) : this(x, 0, 0)
+        {
+        }
+        public Point3D()
+        {
+        }
+        #endregion
+
+
+
+        #region Overriding [ToString, Equals, GetHashCode]
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+
+            Point3D other = (Point3D)obj;
+
+            return this.X == other.X && this.Y == other.Y && this.Z == other.Z;
+        }
+        public override int GetHashCode()
+        {
+            //return base.GetHashCode();
+            return HashCode.Combine(X, Y, Z);
+        }
+        public override string ToString()
+        {
+            return $"Point Coordiantes: ({X},{Y},{Z})";
+        }
+        #endregion
+
+        
+
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using Assignment.First_Task;
+using System;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            #region Task 2, 3 [Casting]
+            /// Point3D p1 = new Point3D()
+            /// {
+            ///     X = Helper.GetIntFromUserUsingTryParse("Point 1 X"),
+            ///     Y = Helper.GetIntFromUserUsingParse("Point 1 Y"),
+            ///     Z = Helper.GetIntFromUserUsingConvert("Point 1 Z")
+            /// };
+            /// Console.WriteLine("----------------------------------------------");
+            /// Point3D p2 = new Point3D()
+            /// {
+            ///     X = Helper.GetIntFromUserUsingTryParse("Point 2 X"),
+            ///     Y = Helper.GetIntFromUserUsingParse("Point 2 Y"),
+            ///     Z = Helper.GetIntFromUserUsingConvert("Point 2 Z")
+            /// };
+            /// Console.WriteLine(p1.ToString());
+            /// Console.WriteLine(p2.ToString()); 
+            #endregion
+
+
+        }
+    }
+}
